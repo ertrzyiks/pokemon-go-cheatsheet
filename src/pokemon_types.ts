0100ea04dc7015748aab2508a4a639d31dd4a2c7
ertrzyiks <mateusz.derks@gmail.com>
2023-10-29 15:26:33 +0100

    Initial commit

---
+++ src/pokemon_types.ts
@@ -0,0 +1,40 @@
+export type PokemonType =
+  'water' |
+  'fire' |
+  'grass' |
+  'electric' |
+  'ice' |
+  'fighting' |
+  'poison' |
+  'ground' |
+  'flying' |
+  'psychic' |
+  'bug' |
+  'rock' |
+  'ghost' |
+  'dark' |
+  'dragon' |
+  'fairy' |
+  'steel' |
+  'normal'
+
+export const allTypes = [
+  'water',
+  'fire',
+  'grass',
+  'electric',
+  'ice',
+  'fighting',
+  'poison',
+  'ground',
+  'flying',
+  'psychic',
+  'bug',
+  'rock',
+  'ghost',
+  'dark',
+  'dragon',
+  'fairy',
+  'steel',
+  'normal',
+] as const
