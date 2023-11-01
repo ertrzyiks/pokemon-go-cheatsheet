48cccb0399db619ba4b2df45f73d00fbbfa966c9
ertrzyiks <mateusz.derks@gmail.com>
2023-11-01 08:38:02 +0100

    Add pills

--- src/pokemon_types_config.tsx
+++ src/pokemon_types_config.tsx
@@ -22,79 +22,98 @@ import waterImage from "./assets/Water.png";
 interface PokemonTypeConfig {
   image: string;
   color: string;
+  label: string;
 }
 
 export const pokemonTypesConfig: Record<PokemonType, PokemonTypeConfig> = {
   'bug': {
     image: bugImage.src,
     color: '#92bc2c',
+    label: 'Bug'
   },
   'dark': {
     image: darkImage.src,
     color: '#595761',
+    label: 'Dark'
   },
   'dragon': {
     image: dragonImage.src,
-    color: '#0C69C8'
+    color: '#0C69C8',
+    label: 'Dragon'
   },
   'electric': {
     image: electricImage.src,
     color: '#F2D94E',
+    label: 'Electric'
   },
   'fairy': {
     image: fairyImage.src,
     color: '#EE90E6',
+    label: 'Fairy'
   },
   'fighting': {
     image: fightingImage.src,
     color: '#D3425F',
+    label: 'Fighting'
   },
   'fire': {
     image: fireImage.src,
     color: '#FBA54C',
+    label: 'Fire'
   },
   'flying': {
     image: flyingImage.src,
     color: '#A1BBEC',
+    label: 'Flying'
   },
   'ghost': {
     image: ghostImage.src,
     color: '#5F6DBC',
+    label: 'Ghost'
   },
   'grass': {
     image: grassImage.src,
     color: '#5FBD58',
+    label: 'Grass'
   },
   'ground': {
     image: groundImage.src,
     color: '#DA7C4D',
+    label: 'Ground'
   },
   'ice': {
     image: iceImage.src,
     color: '#75D0C1',
+    label: 'Ice'
   },
   'normal': {
     image: normalImage.src,
     color: '#A0A29F',
+    label: 'Normal'
   },
   'poison': {
     image: poisonImage.src,
     color: '#B763CF',
+    label: 'Poison'
   },
   'psychic': {
     image: psychicImage.src,
     color: '#FA8581',
+    label: 'Psychic'
   },
   'rock': {
     image: rockImage.src,
     color: '#C9BB8A',
+    label: 'Rock'
   },
   'steel': {
     image: steelImage.src,
     color: '#5695A3',
+    label: 'Steel'
   },
   'water': {
     image: waterImage.src,
     color: '#539DDF',
+    label: 'Water'
   }
 }
