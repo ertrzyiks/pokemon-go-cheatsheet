a51dce078455ae36bb9f38c6e83264a1e56117aa
Joanna Derks <joanna.derks@schibsted.pl>
2023-11-05 17:31:36 +0100

    grunt counter pages

--- src/grunt_catchphrases.ts
+++ src/grunt_catchphrases.ts
@@ -1,4 +1,4 @@
-export const gruntCatchphrases = [
+export const gruntCatchphrasesOld = [
     'Go, my super bug Pokémon!',
     'Wherever there is light, there is also shadow.',
     'ROAR! ...How\'d that sound?',
@@ -18,3 +18,24 @@ export const gruntCatchphrases = [
     'You\'re no match for my iron will!',
     'These waters are treacherous!'
   ] as const
+
+  export const gruntCatchphrases = {
+      'bug': 'Go, my super bug Pokémon!',
+      'dark': 'Wherever there is light, there is also shadow.',
+      'dragon': 'ROAR! ...How\'d that sound?',
+      'electric': 'Get ready to be shocked!',
+      'fairy': 'Check out my cute Pokémon!',
+      'fighting': 'This buff physique isn\'t just for show!',
+      'fire': 'Do you know how hot Pokémon fire breath can get?',
+      'flying': 'Battle against my Flying-type Pokémon!',
+      'ghost': 'Ke... ke... ke... ke... ke... ke!',
+      'grass': 'Don\'t tangle with us!',
+      'ground': 'You\'ll be defeated into the ground!',
+      'ice': 'You\'re gonna be frozen in your tracks.',
+      'normal': 'Normal does not mean weak',
+      'poison': 'Coiled and ready to strike!',
+      'psychic': 'Are you scared of psychics that use unseen power?',
+      'rock': 'Let\'s rock and roll!',
+      'steel': 'You\'re no match for my iron will!',
+      'water': 'These waters are treacherous!'
+  }
