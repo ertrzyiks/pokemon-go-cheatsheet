c24009ec7bc26d48095f2e5b5319b548a37a6bd3
Joanna Derks <joanna.derks@schibsted.pl>
2023-11-17 13:28:39 +0100

    Added basic Grunts to nav

--- src/grunt_catchphrases.ts
+++ src/grunt_catchphrases.ts
@@ -1,26 +1,5 @@
 import { PokemonType } from "./pokemon_types";
 
-  export const gruntCatchphrases_old: Record<PokemonType, string> = {
-      'bug': 'Go, my super bug Pokémon!',
-      'dark': 'Wherever there is light, there is also shadow.',
-      'dragon': 'ROAR! ...How\'d that sound?',
-      'electric': 'Get ready to be shocked!',
-      'fairy': 'Check out my cute Pokémon!',
-      'fighting': 'This buff physique isn\'t just for show!',
-      'fire': 'Do you know how hot Pokémon fire breath can get?',
-      'flying': 'Battle against my Flying-type Pokémon!',
-      'ghost': 'Ke... ke... ke... ke... ke... ke!',
-      'grass': 'Don\'t tangle with us!',
-      'ground': 'You\'ll be defeated into the ground!',
-      'ice': 'You\'re gonna be frozen in your tracks.',
-      'normal': 'Normal does not mean weak',
-      'poison': 'Coiled and ready to strike!',
-      'psychic': 'Are you scared of psychics that use unseen power?',
-      'rock': 'Let\'s rock and roll!',
-      'steel': 'You\'re no match for my iron will!',
-      'water': 'These waters are treacherous!'
-  }
-
   export const gruntCatchphrases: { type: PokemonType, text: string }[] = [
     {type: 'bug', text: 'Go, my super bug Pokémon!'},
     {type: 'dark', text: 'Wherever there is light, there is also shadow.'},
@@ -41,3 +20,13 @@ import { PokemonType } from "./pokemon_types";
 {type: 'steel', text: 'You\'re no match for my iron will!'},
 {type: 'water', text: 'These waters are treacherous!'}
   ]
+
+  export const gruntCatchphrasesInOrder: { type: PokemonType, text: string }[] = gruntCatchphrases.sort((a, b) => {
+    if (a.text < b.text) {
+      return -1;
+    }
+    if (a.text > b.text) {
+      return 1;
+    }
+    return 0;
+  } )
