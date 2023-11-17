f4d4725bc0c7e101df79dd31b57c15bece20349b
Joanna Derks <joanna.derks@schibsted.pl>
2023-11-17 13:45:36 +0100

    Team r styles (#5)
    * team-r styles

--- src/grunt_catchphrases.ts
+++ src/grunt_catchphrases.ts
@@ -1,22 +1,32 @@
 import { PokemonType } from "./pokemon_types";
 
-  export const gruntCatchphrases: Record<PokemonType, string> = {
-      'bug': 'Go, my super bug Pokémon!',
-      'dark': 'Wherever there is light, there is also shadow.',
-      'dragon': 'ROAR! ...How\'d that sound?',
-      'electric': 'Get ready to be shocked!',
-      'fairy': 'Check out my cute Pokémon!',
-      'fighting': 'This buff physique isn\'t just for show!',
-      'fire': 'Do you know how hot Pokémon fire breath can get?',
-      'flying': 'Battle against my Flying-type Pokémon!',
-      'ghost': 'Ke... ke... ke... ke... ke... ke!',
-      'grass': 'Don\'t tangle with us!',
-      'ground': 'You\'ll be defeated into the ground!',
-      'ice': 'You\'re gonna be frozen in your tracks.',
-      'normal': 'Normal does not mean weak',
-      'poison': 'Coiled and ready to strike!',
-      'psychic': 'Are you scared of psychics that use unseen power?',
-      'rock': 'Let\'s rock and roll!',
-      'steel': 'You\'re no match for my iron will!',
-      'water': 'These waters are treacherous!'
-  }
+  export const gruntCatchphrases: { type: PokemonType, text: string }[] = [
+    {type: 'bug', text: 'Go, my super bug Pokémon!'},
+    {type: 'dark', text: 'Wherever there is light, there is also shadow.'},
+    { type: 'dragon', text: 'ROAR! ...How\'d that sound?'},
+    { type: 'electric', text: 'Get ready to be shocked!'},
+    {type: 'fairy', text: 'Check out my cute Pokémon!'},
+{type: 'fighting', text: 'This buff physique isn\'t just for show!'},
+{type: 'fire', text: 'Do you know how hot Pokémon fire breath can get?'},
+{type: 'flying', text: 'Battle against my Flying-type Pokémon!'},
+{type: 'ghost', text: 'Ke... ke... ke... ke... ke... ke!'},
+{type: 'grass', text: 'Don\'t tangle with us!'},
+{type: 'ground', text: 'You\'ll be defeated into the ground!'},
+{type: 'ice', text: 'You\'re gonna be frozen in your tracks.'},
+{type: 'normal', text: 'Normal does not mean weak'},
+{type: 'poison', text: 'Coiled and ready to strike!'},
+{type: 'psychic', text: 'Are you scared of psychics that use unseen power?'},
+{type: 'rock', text: 'Let\'s rock and roll!'},
+{type: 'steel', text: 'You\'re no match for my iron will!'},
+{type: 'water', text: 'These waters are treacherous!'}
+  ]
+
+  export const gruntCatchphrasesInOrder: { type: PokemonType, text: string }[] = gruntCatchphrases.sort((a, b) => {
+    if (a.text < b.text) {
+      return -1;
+    }
+    if (a.text > b.text) {
+      return 1;
+    }
+    return 0;
+  } )
