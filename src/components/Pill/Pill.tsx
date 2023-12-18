c76bc791126217a05b4126ff2a533bf1baeabe31
ertrzyiks <mateusz.derks@gmail.com>
2023-12-18 21:23:07 +0100

    Remove tap highlight from switch

--- src/components/Pill/Pill.tsx
+++ src/components/Pill/Pill.tsx
@@ -1,3 +1,4 @@
+import { clsx } from "clsx";
 import "./styles.css";
 
 interface BaseProps {}
@@ -17,17 +18,22 @@ const Pill = (props: PillProps) => {
   if (props.as === "button") {
     const { as, ...rest } = props;
     return (
-      <button {...rest} className={`pill is-interactive ${rest.className}`} />
+      <button
+        {...rest}
+        className={clsx(["pill is-interactive", rest.className])}
+      />
     );
   }
 
   if (props.as === "link") {
     const { as, ...rest } = props;
-    return <a {...rest} className={`pill is-interactive ${rest.className}`} />;
+    return (
+      <a {...rest} className={clsx(["pill is-interactive", rest.className])} />
+    );
   }
 
   const { as, ...rest } = props;
-  return <div {...rest} className={`pill ${rest.className}`} />;
+  return <div {...rest} className={clsx(["pill", rest.className])} />;
 };
 
 export default Pill;
