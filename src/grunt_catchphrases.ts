b8b13b0d515d8c69a2088ceae767e4ebb862efa0
ertrzyiks <mateusz.derks@gmail.com>
2024-01-27 15:42:33 +0100

    Fix eslint issue

--- src/grunt_catchphrases.ts
+++ src/grunt_catchphrases.ts
@@ -1,27 +1,31 @@
-import { PokemonType } from "./pokemon_types";
+import type { PokemonType } from "./pokemon_types";
 
-  export const gruntCatchphrases: { type: PokemonType, text: string }[] = [
-    {type: 'bug', text: 'Go, my super bug Pokémon!'},
-    {type: 'dark', text: 'Wherever there is light, there is also shadow.'},
-    { type: 'dragon', text: 'ROAR! ...How\'d that sound?'},
-    { type: 'electric', text: 'Get ready to be shocked!'},
-    {type: 'fairy', text: 'Check out my cute Pokémon!'},
-{type: 'fighting', text: 'This buff physique isn\'t just for show!'},
-{type: 'fire', text: 'Do you know how hot Pokémon fire breath can get?'},
-{type: 'flying', text: 'Battle against my Flying-type Pokémon!'},
-{type: 'ghost', text: 'Ke... ke... ke... ke... ke... ke!'},
-{type: 'grass', text: 'Don\'t tangle with us!'},
-{type: 'ground', text: 'You\'ll be defeated into the ground!'},
-{type: 'ice', text: 'You\'re gonna be frozen in your tracks.'},
-{type: 'normal', text: 'Normal does not mean weak'},
-{type: 'poison', text: 'Coiled and ready to strike!'},
-{type: 'psychic', text: 'Are you scared of psychics that use unseen power?'},
-{type: 'rock', text: 'Let\'s rock and roll!'},
-{type: 'steel', text: 'You\'re no match for my iron will!'},
-{type: 'water', text: 'These waters are treacherous!'}
-  ]
+export const gruntCatchphrases: { type: PokemonType; text: string }[] = [
+  { type: "bug", text: "Go, my super bug Pokémon!" },
+  { type: "dark", text: "Wherever there is light, there is also shadow." },
+  { type: "dragon", text: "ROAR! ...How'd that sound?" },
+  { type: "electric", text: "Get ready to be shocked!" },
+  { type: "fairy", text: "Check out my cute Pokémon!" },
+  { type: "fighting", text: "This buff physique isn't just for show!" },
+  { type: "fire", text: "Do you know how hot Pokémon fire breath can get?" },
+  { type: "flying", text: "Battle against my Flying-type Pokémon!" },
+  { type: "ghost", text: "Ke... ke... ke... ke... ke... ke!" },
+  { type: "grass", text: "Don't tangle with us!" },
+  { type: "ground", text: "You'll be defeated into the ground!" },
+  { type: "ice", text: "You're gonna be frozen in your tracks." },
+  { type: "normal", text: "Normal does not mean weak" },
+  { type: "poison", text: "Coiled and ready to strike!" },
+  {
+    type: "psychic",
+    text: "Are you scared of psychics that use unseen power?",
+  },
+  { type: "rock", text: "Let's rock and roll!" },
+  { type: "steel", text: "You're no match for my iron will!" },
+  { type: "water", text: "These waters are treacherous!" },
+];
 
-  export const gruntCatchphrasesInOrder: { type: PokemonType, text: string }[] = gruntCatchphrases.sort((a, b) => {
+export const gruntCatchphrasesInOrder: { type: PokemonType; text: string }[] =
+  gruntCatchphrases.sort((a, b) => {
     if (a.text < b.text) {
       return -1;
     }
@@ -29,4 +33,4 @@ import { PokemonType } from "./pokemon_types";
       return 1;
     }
     return 0;
-  } )
+  });
