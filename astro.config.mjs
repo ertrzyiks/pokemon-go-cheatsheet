0bd2b82696a42afd19b6d06ae4a1a4c9410b7bbe
Mateusz Derks <mateusz.derks@gmail.com>
2025-03-16 20:55:49 +0100

    Add sitemap (#24)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pokemon-go-cheatsheet.ertrzyiks.me",
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()]
 });
